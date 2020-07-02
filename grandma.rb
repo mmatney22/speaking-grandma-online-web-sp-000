4744fb77d84a374ef04101d4500dc825b12fa0ea
Morgan Matney <morgan.matney89@gmail.com>
2020-07-02 18:57:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if 
+  if
     puts
-  elsif 
-    puts  
-  
+  elsif
+    puts
+
   else
     puts "HUH?!, SPEAK UP, SONNY!"
   end
